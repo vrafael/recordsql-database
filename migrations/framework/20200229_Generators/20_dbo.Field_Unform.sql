2612f611dec6b85389c29544ef567ab04d5bdaba
rafaelvaliullin <vrafael@mail.ru>
2020-09-26 22:16:27 +0300

    feat(Generators): Regenerate type when field forming

---
+++ migrations/framework/20200229_Generators/20_dbo.Field_Unform.sql
@@ -0,0 +1,59 @@
+--liquibase formatted sql
+
+--changeset vrafael:framework_20200229_Generators_20_dboFieldUnform logicalFilePath:path-independent splitStatements:true stripComments:false endDelimiter:\nGO runOnChange:true
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+--------- framework "RecordSQL" v2 (https://github.com/vrafael/recordsql-db) ---------
+CREATE OR ALTER PROCEDURE [dbo].[FieldUnform]
+    @ID bigint
+   ,@TransitionID bigint = NULL
+AS
+EXEC [dbo].[ContextProcedurePush]
+    @ProcID = @@PROCID
+BEGIN
+    SET NOCOUNT ON
+
+    DECLARE 
+        @FieldOwnerID bigint
+       ,@StateID_Basic_Formed bigint = dbo.DirectoryIDByOwner(N'State', N'Basic', N'Formed')
+       ,@TypeID bigint
+
+    SELECT
+        @FieldOwnerID = o.OwnerID
+    FROM dbo.TObject o 
+    WHERE o.ID = @ID
+
+    --переформировываем владельца поля и его дочерние типы
+    IF (dbo.ObjectStateTag(@FieldOwnerID) = N'Formed')
+    BEGIN
+        DECLARE cur CURSOR LOCAL STATIC FORWARD_ONLY FOR
+            SELECT ct.ID
+            FROM dbo.DirectoryChildrenInline(@FieldOwnerID, N'Type', 1) ct
+                JOIN dbo.TObject ot ON ot.ID = ct.ID
+            WHERE ot.StateID = @StateID_Basic_Formed
+            ORDER BY ct.Lvl
+
+        OPEN cur
+
+        --расформировывается владелец и его дочерние типы
+        EXEC dbo.ObjectStatePush
+            @ID = @FieldOwnerID
+
+        --формируем типы начиная с верхнего уровня
+        FETCH NEXT FROM cur INTO @TypeID
+        
+        WHILE @@FETCH_STATUS = 0
+        BEGIN
+            EXEC dbo.ObjectStatePush
+                @ID = @TypeID
+               ,@StateID = @StateID_Basic_Formed
+
+            FETCH NEXT FROM cur INTO @TypeID
+        END
+        
+        CLOSE cur
+        DEALLOCATE cur
+    END
+END
+--EXEC dbo.FieldUnform
--- migrations/framework/20200229_Generators/21_TypeBasicLinks.sql
+++ migrations/framework/20200229_Generators/21_TypeBasicLinks.sql
@@ -5,9 +5,13 @@ DECLARE
     @TransitionID_Basic_Form bigint = dbo.DirectoryIDByOwner(N'Transition', N'Basic', N'Form')
    ,@TransitionID_Basic_Unform bigint = dbo.DirectoryIDByOwner(N'Transition', N'Basic', N'Unform')
    ,@TypeID_Type bigint = dbo.TypeIDByTag(N'Type')
+   ,@TypeID_Field bigint = dbo.TypeIDByTag(N'Field')
    ,@StoredProcedureID_dbo_TypeForm bigint = dbo.DirectoryIDByOwner(N'StoredProcedure', N'dbo', N'TypeForm')
    ,@StoredProcedureID_dbo_TypeUnform bigint = dbo.DirectoryIDByOwner(N'StoredProcedure', N'dbo', N'TypeUnform')
+   ,@StoredProcedureID_dbo_FieldForm bigint = dbo.DirectoryIDByOwner(N'StoredProcedure', N'dbo', N'FieldForm')
+   ,@StoredProcedureID_dbo_FieldUnform bigint = dbo.DirectoryIDByOwner(N'StoredProcedure', N'dbo', N'FieldUnform')
 
+-----------Types-----------
 --добавляем ссылку на переход Form 
 IF NOT EXISTS
 (
@@ -43,5 +47,44 @@ BEGIN
        ,@OwnerID = @TransitionID_Basic_Unform
        ,@TargetID = @StoredProcedureID_dbo_TypeUnform
        ,@CaseID = @TypeID_Type
+       ,@Order = 1
+END
+
+-----------Fields-----------
+--добавляем ссылку на переход Form 
+IF NOT EXISTS
+(
+    SELECT 1
+    FROM dbo.TLink l
+    WHERE l.OwnerID = @TransitionID_Basic_Form
+        AND l.CaseID = @TypeID_Field
+        AND l.TargetID = @StoredProcedureID_dbo_FieldForm
+)
+BEGIN
+    EXEC dbo.LinkSet
+        @LinkID = NULL
+       ,@TypeTag = N'LinkToStoredProcedureOnTransition'
+       ,@OwnerID = @TransitionID_Basic_Form
+       ,@CaseID = @TypeID_Field
+       ,@Order = 1
+       ,@TargetID = @StoredProcedureID_dbo_FieldForm
+END
+
+--добавляем ссылку на переход Unform
+IF NOT EXISTS
+(
+    SELECT 1
+    FROM dbo.TLink l
+    WHERE l.OwnerID = @TransitionID_Basic_Unform
+        AND l.CaseID = @TypeID_Field
+        AND l.TargetID = @StoredProcedureID_dbo_FieldUnform
+)
+BEGIN
+    EXEC dbo.LinkSet
+        @LinkID = NULL
+       ,@TypeTag = N'LinkToStoredProcedureOnTransition'
+       ,@OwnerID = @TransitionID_Basic_Unform
+       ,@TargetID = @StoredProcedureID_dbo_FieldUnform
+       ,@CaseID = @TypeID_Field
        ,@Order = 1
 END
